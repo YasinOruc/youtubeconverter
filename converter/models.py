e14ace462b89c18f9fe249c3fb207da7913118ef
YasinOruc <orucyasin14@gmail.com>
2024-10-27 18:53:02 +0100

    converter models

--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,8 @@
+# converter/models.py
 from django.db import models
 
-# Create your models here.
+class Download(models.Model):
+    url = models.URLField()
+    file_format = models.CharField(max_length=10)  # 'mp3' of 'mp4'
+    status = models.CharField(max_length=20, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
